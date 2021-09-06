38612821a0ae2359d262beab536ff842f8f18206
hootrix <hhtjim@gmail.com>
2021-09-07 01:30:27 +0800

    debug

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ with open(conf_path) as f:
 cache = diskcache.Cache(parent_path+'/.tmp')# 设置缓存文件目录  当前tmp文件夹。用于缓存分步执行命令的操作，避免bot无法找到当前输入操作的进度
 client = TelegramClient('{}/.{}_tg_login'.format(parent_path,account['username']), account['api_id'], account['api_hash'], proxy = proxy)
 client.start(phone=account['phone'])
-client.start()
+# client.start()
 
 # 设置bot，且直接启动
 bot = TelegramClient('.{}'.format(account['bot_name']), account['api_id'], account['api_hash'],proxy = proxy).start(bot_token=account['bot_token'])
